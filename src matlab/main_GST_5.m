42e4c4e818fddb86b45644e335dc907cfce028ea
Vlad Karpushin <karpushin@ngs.ru>
2018-08-21 12:48:05 +0700

    matlab: changed variable names

--- src matlab/main_GST_5.m
+++ src matlab/main_GST_5.m
@@ -4,7 +4,8 @@
 close all,clc,clear all;
 
 strFolder = 'D:\home\programming\vc\new\6_My home projects\4_GST\input\';
-strFileName = strcat(strFolder,'segm1.bmp');
+%strFileName = strcat(strFolder,'segm1.bmp');
+strFileName = strcat(strFolder,'6.bmp');
 
 %****************************
 %*****  input image  ********
@@ -22,8 +23,8 @@ SizeRad = 21;
 %*****GST calculation********
 %****************************
 
-[StatSegment1, StatSegment2, StatSegment3, AngGr] = CalcGST(img, SizeRad, 'msobel');
-imgBin = StatSegment2 > 0.55;
+[imgCoherency1, imgCoherency2, imgCoherency3, imgOrientation] = CalcGST(img, SizeRad, 'msobel');
+imgBin = imgCoherency2 > 0.55;
 
 %*******************
 %*****Output********
@@ -35,21 +36,21 @@ imshow(img);
 title('original');
 
 subplot(2,3,2);
-imshow(StatSegment1,[]);
-title('StatSegment1=1-lambda2./lambda1');
+imshow(imgCoherency1,[]);
+title('C1=1-lambda2./lambda1');
 
 subplot(2,3,3);
-imshow(StatSegment2,[]);
-title('StatSegment2=(lambda1 - lambda2)./(lambda1+lambda2)');
+imshow(imgCoherency2,[]);
+title('C2=(lambda1 - lambda2)./(lambda1+lambda2)');
 
 subplot(2,3,4);
-imshow(StatSegment3,[]);
-title('StatSegment3=((lambda1 - lambda2)./(lambda1+lambda2))^2');
+imshow(imgCoherency3,[]);
+title('C3=((lambda1 - lambda2)./(lambda1+lambda2))^2');
 
 subplot(2,3,5);
 imshow(imgBin,[]);
 title('imgBin');
 
 subplot(2,3,6);
-imshow(AngGr,[]);
+imshow(imgOrientation,[]);
 title('Angle');

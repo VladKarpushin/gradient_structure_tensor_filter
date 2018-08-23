3d5b3c06ef36a017d60f6590677e0c4067cd2871
Vlad Karpushin <karpushin@ngs.ru>
2018-08-23 15:03:38 +0700

    matlab: added imgBin output

--- src matlab/main_GST_5.m
+++ src matlab/main_GST_5.m
@@ -6,8 +6,8 @@
 close all,clc,clear all;
 
 strFolder = 'D:\home\programming\vc\new\6_My home projects\4_GST\input\';
-%strFileName = strcat(strFolder,'segm1.bmp');
-strFileName = strcat(strFolder,'6.bmp');
+strFileName = strcat(strFolder,'segm1.bmp');
+%strFileName = strcat(strFolder,'6.bmp');
 
 %****************************
 %*****  input image  ********
@@ -19,40 +19,46 @@ if size(img,3)==3
     img = rgb2gray(img);    
 end
 
-SizeRad = 21;       %radius
+SizeRad = 25;       %radius
 
 %****************************
 %*****GST calculation********
 %****************************
 
 [imgCoherency1, imgCoherency2, imgCoherency3, imgOrientation] = CalcGST(img, SizeRad, 'msobel');
-imgBin = imgCoherency2 > 0.55;
+imgCoherencyBin = imgCoherency2 > 0.43;
+imgOrientationBin = imgOrientation > 35 & imgOrientation < 57;
+imgBin = imgCoherencyBin & imgOrientationBin;
 
 %*******************
 %*****Output********
 %*******************
 
 figure, 
-subplot(2,3,1);
+subplot(2,4,1);
 imshow(img);
 title('original');
 
-subplot(2,3,2);
+subplot(2,4,2);
 imshow(imgCoherency1,[]);
 title('C1=1-lambda2./lambda1');
 
-subplot(2,3,3);
+subplot(2,4,3);
 imshow(imgCoherency2,[]);
 title('C2=(lambda1 - lambda2)./(lambda1+lambda2)');
 
-subplot(2,3,4);
+subplot(2,4,4);
 imshow(imgCoherency3,[]);
 title('C3=((lambda1 - lambda2)./(lambda1+lambda2))^2');
 
-subplot(2,3,5);
-imshow(imgBin,[]);
-title('imgBin');
+subplot(2,4,5);
+imshow(imgCoherencyBin,[]);
+title('imgCoherencyBin');
 
-subplot(2,3,6);
+subplot(2,4,6);
 imshow(imgOrientation,[]);
 title('Angle');
+
+subplot(2,4,7);
+imshow(imgBin,[]);
+title('imgBin');

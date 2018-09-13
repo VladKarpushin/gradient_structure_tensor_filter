7c5dab2037d7dcacb42d51f84a2cc7df2c0a567c
Vlad Karpushin <karpushin@ngs.ru>
2018-09-13 16:32:56 +0700

    cpp: improved code

--- src cpp/Project1/anisotropic_image_segmentation.cpp
+++ src cpp/Project1/anisotropic_image_segmentation.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
+//#include "opencv2/highgui.hpp"
 
 using namespace cv;
 using namespace std;
